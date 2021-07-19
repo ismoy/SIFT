2194d69d26b65e350c2771fb6e47754c9026db83
ismoy <belizairesmoy72@gmail.com>
2021-07-19 03:13:13 -0400

    cambio de los comentarios del codigo

--- app/src/main/java/cl/tofcompany/sift/Controllers/Clients/UpdateProfile.java
+++ app/src/main/java/cl/tofcompany/sift/Controllers/Clients/UpdateProfile.java
@@ -62,55 +62,97 @@ public class UpdateProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
-        miImageViewprofile = findViewById(R.id.imageviewprofileclient);
-        mButtonupdate = findViewById(R.id.btnupdateprofile);
-        mTextViewname = findViewById(R.id.actualizarname);
-        mTextViewpassword = findViewById(R.id.actualizarpassword);
-        MyToolbar.show(this,"",true);
-        mClientProvider = new ClientProvider();
-        mAuthProvider = new AuthProvider();
-        mImageProvider = new ImagesProvider("client_images");
-        mProgressDialog = new ProgressDialog(this);
-        getClientInfo();
-        mButtonupdate.setOnClickListener(v ->
-                updateProfile());
-        miImageViewprofile.setOnClickListener(v ->
-                openGallery());
+       init();
     }
 
+    //aqui iniciamos los variable y las clases
+    private void init(){
+        //iniciamos el image con su id
+        miImageViewprofile = findViewById(R.id.imageviewprofileclient);
+        //iniciamos el btn update con su id
+        mButtonupdate = findViewById(R.id.btnupdateprofile);
+        //iniciamos el name con su id
+        mTextViewname = findViewById(R.id.actualizarname);
+        //iniciamos el password con su id
+        mTextViewpassword = findViewById(R.id.actualizarpassword);
+        //llamamos nuestra toolbar
+        MyToolbar.show(this,"",true);
+        //iniciamos clase ClientProvider
+        mClientProvider = new ClientProvider();
+        //iniciamos clase AuthProvider
+        mAuthProvider = new AuthProvider();
+        //iniciamos clase ImageProvider y lo pasamos el nodo client_image
+        mImageProvider = new ImagesProvider("client_images");
+        //llamado progresDialog
+        mProgressDialog = new ProgressDialog(this);
+        //metodo para tomar los datos de los clientes
+        getClientInfo();
+        //agregamos un evento onclick en el buton update
+        mButtonupdate.setOnClickListener(v ->
+                //metodo para actualizar
+                updateProfile());
+        //aplicamos un evento onclick sobre el imagen de perfil
+        miImageViewprofile.setOnClickListener(v ->
+                //para abrir la galeria y seleccionar otra foto
+                openGallery());
+    }
+    //metodo para abrir la galeria y cambiar la foto
     private void openGallery() {
+        //creamos un intent de una action_get_content
         Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        //decimos que el tipo va hacer de todos tipos de imagenes
         galleryIntent.setType("image/*");
+        //iniciamos la actividad con el resultado lo pasamos el intent y tb el gallery_request
         startActivityForResult(galleryIntent, GALLERY_REQUEST );
     }
 
     @Override
+    //sobre escribir el metodo de onActivityResult
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //asegurar que el requesCode es igual a mi Gallery_request y si el resultado es igual a ok
         if (requestCode== GALLERY_REQUEST && resultCode == RESULT_OK) {
             try {
+                //a mi variable mimagefile lo pasamos el metodo de FileUtil y ejecutamos el from
+                //recibe un contexto que es this y luego sacamos la data que llego
                 mimagefile = FileUtil.from(this, data.getData());
+                //eso me va a decodificar el imager y me lo va a coprimir para no ocupa muchos espacios
                 miImageViewprofile.setImageBitmap(BitmapFactory.decodeFile(mimagefile.getAbsolutePath()));
             } catch(Exception e) {
                 Log.d("ERROR", "Mensaje: " +e.getMessage());
             }
         }
     }
-
+    //metodo para tomar los infos del cliente
     private void getClientInfo() {
+        //llamamos la clase ClientProvider y ejecutamos el metodo getCLient eso recibe un id
+        //lo tenemos en mAuthProvider y luego lo pasamos un evento de firebase addListenerForSingleValueEvent
         mClientProvider.getClient(mAuthProvider.getId()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //aseguremos que el datos exist
                 if (dataSnapshot.exists()) {
+                    //creamos un variable name y lo pasamos el valor proviene de firebase mediante
+                    //dataSnapshot.child("name")
                     String name = dataSnapshot.child("name").getValue().toString();
+                    //creamos un variable password y lo pasamos el valor proviene de firebase mediante
+                    //dataSnapshot.child("password")
                     String password = dataSnapshot.child("password").getValue().toString();
+                    //creamos un variable password2 y lo pasamos el valor proviene de firebase mediante
+                    //dataSnapshot.child("password2")
                     String password2 = dataSnapshot.child("password2").getValue().toString();
+                    //creamos un variable image lo iniciamos en vacio
                     String image = "";
+                    //aseguramos que el nodo image en firebas tiene imagen
                     if (dataSnapshot.hasChild("image")) {
+                        //mi image lo pasamos el valor que proviene de firebase mediante dataSnapshot.child("image")
                         image = dataSnapshot.child("image").getValue().toString();
+                        //al final lo procesamos el imagen con picasso y luego enviarlo en la vista
                         Picasso.get().load(image).into(miImageViewprofile);
                     }
+                    //enviar el name en la vista
                     mTextViewname.setText(name);
+                    //enviar el password en la vista
                     mTextViewpassword.setText(password);
                 }
             }
@@ -121,39 +163,60 @@ public class UpdateProfile extends AppCompatActivity {
             }
         });
     }
-
+    //metodo para actualizar los datos
     private void updateProfile() {
+        //tomamos el valor que esta dentro en editText
         mname = mTextViewname.getText().toString();
+        //aseguremos que no esta vacio tb en imagefile
         if (!mname.equals("") && mimagefile != null) {
+            //mensaje del dialog
             mProgressDialog.setMessage("Espere un momento...");
+            //desactivamos la opcion del Touch para que el usuario no pueda cancelar esta operacion
             mProgressDialog.setCanceledOnTouchOutside(false);
+            //mestramos el dialog
             mProgressDialog.show();
-
+            //guardamos los datos si todos estan ok
             saveImage();
         }
         else {
+            //mensaje de error
             Toast.makeText(this, "Ingresa la imagen y el nombre", Toast.LENGTH_SHORT).show();
         }
     }
-
+     //metodo para guardar image y nombre despues de modificarlo
     private void saveImage() {
+        //entramos en la clase de ImageProvider ejecutamos el metodo de saveImage eso recibe un contexto
+        //en mi caso sera this, tb recibe um image lo tenemos en mimagefile, tb recibe un id
+        //lo tenemos en mAuthProvider
         mImageProvider.saveImage(UpdateProfile.this, mimagefile, mAuthProvider.getId()).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                //pregunta si la tarea fue exitosa
                 if (task.isSuccessful()) {
+                    //llamamos la clase ImageProvider ejecutamos el metodo getStorage y el getDownloadUrl()
+                    //eso ricibe el uri como parametro
                     mImageProvider.getStorage().getDownloadUrl().addOnSuccessListener(uri -> {
+                        //guardamos en image el uri
                         String image = uri.toString();
+                        //llamamos la clase del modelo cliente
                         Client client = new Client();
+                        //enviamos el imagen en el image
                         client.setImage(image);
+                        //enviamos el nombre en el name
                         client.setName(mname);
+                        //enviamos el id en el mAuthProvider
                         client.setId(mAuthProvider.getId());
+                        //entramos en la clase de ClientProvider y ejecutamos el metodo update eso recibe client por parametro
                         mClientProvider.update(client).addOnSuccessListener(aVoid -> {
+                            //ocultar dialog
                             mProgressDialog.dismiss();
+                            //mensaje exito
                             Toast.makeText(UpdateProfile.this, "Su informacion se actualizo correctamente", Toast.LENGTH_SHORT).show();
                         });
                     });
                 }
                 else {
+                    //,ensaje error
                     Toast.makeText(UpdateProfile.this, "Hubo un error al subir la imagen", Toast.LENGTH_SHORT).show();
                 }
             }
--- app/src/main/java/cl/tofcompany/sift/Controllers/Clients/WalletClient.java
+++ app/src/main/java/cl/tofcompany/sift/Controllers/Clients/WalletClient.java
@@ -14,97 +14,133 @@ import cl.tofcompany.sift.R;
 import cl.tofcompany.sift.includes.MyToolbar;
 
 public class WalletClient extends AppCompatActivity {
+    //variable TextView numerotarjeta
     private TextView mnmerodetarjeta;
+    //variable TextView fecha vencimiento
     private TextView mfechavencimiento;
+    //variable TextView cvv
     private TextView mcodigoseguridad;
+    //variable TextView nombretarjeta
     private TextView mnombretarjeta;
+    //variable View layoutpagocongoogle
     private View layoutpagocongoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet_client);
-        MyToolbar.show(this, "", true);
-        View layoutefectivo = findViewById(R.id.layoutefectivo);
-        layoutefectivo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WalletClient.this, PagoEnEfectivoClient.class);
-                startActivity(intent);
-            }
-        });
-        View layoutvisaclient = findViewById(R.id.layoutvisaclient);
-        layoutvisaclient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences preferencias = getSharedPreferences("datosTarjetas", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferencias.edit();
-                editor.putString("mnumerotarjeta", mnmerodetarjeta.getText().toString());
-                editor.putString("mnombretarjeta", mnombretarjeta.getText().toString());
-                editor.putString("mfechavencimiento", mfechavencimiento.getText().toString());
-                editor.putString("mcodigoseguridad", mcodigoseguridad.getText().toString());
-                editor.commit();
-                Intent intent = new Intent(WalletClient.this, TarjetaAgregadoClient.class);
-                startActivity(intent);
-            }
-        });
-        View layoutagregartarjetas = findViewById(R.id.layoutagregartarjetas);
-        layoutagregartarjetas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WalletClient.this, PagoActivity.class);
-                startActivity(intent);
-            }
-        });
-        TextView textagregartarjetas = findViewById(R.id.textagregartarjetas);
-        textagregartarjetas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WalletClient.this, PagoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        View layoutcupones = findViewById(R.id.layoutcupones);
-        layoutcupones.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(WalletClient.this, CuponesDisponibleClient.class);
-                startActivity(intent);
-            }
-        });
-        mnmerodetarjeta = findViewById(R.id.tarjetayaagregada);
-        mnombretarjeta = findViewById(R.id.nombretarjetadesing);
-        mcodigoseguridad = findViewById(R.id.codigoseguridadtarjetadesing);
-        mfechavencimiento = findViewById(R.id.fechavencimientodesing);
-        recibirdatosdetarjetas();
         init();
     }
+    //aqui esta las inicializaciones de los variables y layout
+   private void init(){
+       //lammando a mi toolbar
+       MyToolbar.show(this, "", true);
+       //iniciando el view de layoutefectivo con su id
+       View layoutefectivo = findViewById(R.id.layoutefectivo);
+       //agregamos un onclick sobre el layout
+       layoutefectivo.setOnClickListener(v -> {
+           //creamos un intent
+           Intent intent = new Intent(WalletClient.this, PagoEnEfectivoClient.class);
+           //iniciamos el intent
+           startActivity(intent);
+       });
+       //iniciando el view de layoutvisaclient con su id
+       View layoutvisaclient = findViewById(R.id.layoutvisaclient);
+       //agregamos un onclik sobre el layout
+       layoutvisaclient.setOnClickListener(v -> {
+           //recibir datos de la tarjeta mediante SharedPreferences
+           SharedPreferences preferencias = getSharedPreferences("datosTarjetas", Context.MODE_PRIVATE);
+           //invocamos el editos de SharedPreferences
+           SharedPreferences.Editor editor = preferencias.edit();
+           //enviamos el numero de tarjeta recibids
+           editor.putString("mnumerotarjeta", mnmerodetarjeta.getText().toString());
+           //enviamos el nombre de tarjeta recibids
+           editor.putString("mnombretarjeta", mnombretarjeta.getText().toString());
+           //enviamos el fechavencimiento de tarjeta recibids
+           editor.putString("mfechavencimiento", mfechavencimiento.getText().toString());
+           //enviamos el cvv de tarjeta recibids
+           editor.putString("mcodigoseguridad", mcodigoseguridad.getText().toString());
+           //aplicamos los cambios
+           editor.apply();
+           //creamos el intent donde van a recibir esos datos
+           Intent intent = new Intent(WalletClient.this, TarjetaAgregadoClient.class);
+           //iniciamos el intent
+           startActivity(intent);
+       });
+       //iniciando el view de layoutagregartarjetas con su id
+       View layoutagregartarjetas = findViewById(R.id.layoutagregartarjetas);
+       //agregamos un onclick sobre el layout
+       layoutagregartarjetas.setOnClickListener(v -> {
+           //creamos un intent
+           Intent intent = new Intent(WalletClient.this, PagoActivity.class);
+           //iniciamos el intent
+           startActivity(intent);
+       });
+       //iniciando el TextView de agregartarjeta con su id
+       TextView textagregartarjetas = findViewById(R.id.textagregartarjetas);
+       //agregamos un onclik sobre el TextView
+       textagregartarjetas.setOnClickListener(v -> {
+           //creamos un intent
+           Intent intent = new Intent(WalletClient.this, PagoActivity.class);
+           //iniciamos el intent
+           startActivity(intent);
+       });
 
-    private void init() {
-        layoutpagocongoogle = findViewById(R.id.layoutpagocongoogle);
-        layoutpagocongoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               Toast.makeText(WalletClient.this, "Proximamente", Toast.LENGTH_SHORT).show();
-                // startActivity(new Intent(WalletClient.this,itempagarcongoogle.class));
-            }
-        });
-    }
-
+       //iniciando el view de layoutcupones con su id
+       View layoutcupones = findViewById(R.id.layoutcupones);
+       //agregamos un onclick sobre el layout
+       layoutcupones.setOnClickListener(v -> {
+           //creamos un intent
+           Intent intent = new Intent(WalletClient.this, CuponesDisponibleClient.class);
+           //iniciamos el intent
+           startActivity(intent);
+       });
+       //iniciamos el numero de tarjeta con su id
+       mnmerodetarjeta = findViewById(R.id.tarjetayaagregada);
+       //iniciamos el nombre de tarjeta con su id
+       mnombretarjeta = findViewById(R.id.nombretarjetadesing);
+       //iniciamos el cvv de tarjeta con su id
+       mcodigoseguridad = findViewById(R.id.codigoseguridadtarjetadesing);
+       //iniciamos la fechavencimiento de tarjeta con su id
+       mfechavencimiento = findViewById(R.id.fechavencimientodesing);
+       //iniciando el view de layoutpagocongoogle con su id
+       layoutpagocongoogle = findViewById(R.id.layoutpagocongoogle);
+       //agregamos un onclick sobre el layout
+       layoutpagocongoogle.setOnClickListener(v -> {
+           //mensaje disponibilidad
+           Toast.makeText(WalletClient.this, "Proximamente", Toast.LENGTH_SHORT).show();
+           // startActivity(new Intent(WalletClient.this,itempagarcongoogle.class));
+       });
+       //metdo que recibe los datos de la tarjeta
+       recibirdatosdetarjetas();
+   }
+      //metodo para recibir los datos de la tarjeta
     private void recibirdatosdetarjetas() {
+        //recibir los datos de la tarjeta mediante SharedPreferences
         SharedPreferences preferencias = getSharedPreferences("datosTarjetas", Context.MODE_PRIVATE);
+        //crear una variable llegotarjeta y lo pasamos el numero de tarjeta que proviene de preferencias
+        // y tb lo pasamos un valor default por si no hay en mi caso puse No hay tarjetas igual puedes dejarlo vacio
         String llegotarjeta = preferencias.getString("mnumerotarjeta", "No hay tarjetas");
+        //crear una variable llegofechavencimiento y lo pasamos la fechavencimiento de la tarjeta que proviene de preferencias
+        // y tb lo pasamos un valor default en mi caso esta vacio
         String llegofechavencimiento = preferencias.getString("mfechavencimiento", "");
+        //crear una variable llegocodigoseguridad y lo pasamos el cvv de la tarjeta que proviene de preferencias
+        // y tb lo pasamos un valor default en mi caso esta vacio
         String llegocodigoseguridad = preferencias.getString("mcodigoseguridad", "");
+        //crear una variable llegonombre y lo pasamos el nombre de la tarjeta que proviene de preferencias
+        // y tb lo pasamos un valor default en mi caso esta vacio
         String llegonombre = preferencias.getString("mnombretarjeta", "");
+        //invocamos el editor
         SharedPreferences.Editor editor = preferencias.edit();
+        //aplicamos el cambio
         editor.apply();
+        //enviamos en la vista lo que recibe llegotarjeta
         mnmerodetarjeta.setText(llegotarjeta);
+        //enviamos en la vista lo que recibe llegofechavencimiento
         mfechavencimiento.setText(llegofechavencimiento);
+        //enviamos en la vista lo que recibe llegonombre
         mnombretarjeta.setText(llegonombre);
+        //enviamos en la vista lo que recibe llegocodigoseguridad
         mcodigoseguridad.setText(llegocodigoseguridad);
 
 
